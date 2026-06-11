f1c3708b2da564a5d20978733a47526d2a9c9804
ellenoireQ <ketchupnaiso@gmail.com>
2026-06-12 06:28:13 +0700

    fix: allow window to scretching

--- main.cpp
+++ main.cpp
@@ -157,7 +157,6 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
 
     GLFWwindow *window = glfwCreateWindow(150, 50, "LearnOpenGL Window", NULL, NULL);
     if (window == NULL)
